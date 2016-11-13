df12a04200d586cbfae87406a1ad230045cd877b
Jana <jana.stc@gmail.com>
2016-11-13 11:12:16 -0500

    Modified bad urls to always redirect to home

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^(?P<project_name>[-_: a-zA-Z0-9]*)/$', views.project, name='project'),
-    url(r'^$', views.index, name='index'),
+    # url(r'^(?P<project_name>[-_: a-zA-Z0-9]*)/$', views.project, name='project'),
+    url(r'^', views.index, name='index'),
 ]
